7c377944a65c4de968ee5986cc55e7f1b618524d
Carlo Pinciroli <ilpincy@gmail.com>
2012-11-03 17:57:41 +0100

    Moved cmake build checks in a separate file Added a facility variable to check if it's a simulation build or not

---
+++ src/cmake/ARGoSBuildChecks.cmake
@@ -0,0 +1,51 @@
+#
+# Check for dynamic library loading facility
+#
+if(ARGOS_DYNAMIC_LIBRARY_LOADING)
+  find_package(DLFCN)
+  if(NOT DLFCN_FOUND)  
+    message(FATAL_ERROR "Required library pthreads not found.")
+  endif(NOT DLFCN_FOUND)
+  add_definitions(-DARGOS_DYNAMIC_LIBRARY_LOADING)
+  include_directories(${DLFCN_INCLUDE_DIR})
+endif(ARGOS_DYNAMIC_LIBRARY_LOADING)
+
+#
+# Check for PThreads
+# It is required only when compiling the simulator
+#
+if(ARGOS_BUILD_FOR_SIMULATOR)
+  find_package(Pthreads)
+  if(NOT PTHREADS_FOUND)  
+    message(FATAL_ERROR "Required library pthreads not found.")
+  endif(NOT PTHREADS_FOUND)
+  add_definitions(${PTHREADS_DEFINITIONS})
+  include_directories(${PTHREADS_INCLUDE_DIR})
+endif(ARGOS_BUILD_FOR_SIMULATOR)
+
+#
+# Check for GSL
+# It is required only when compiling the simulator
+#
+find_package(GSL)
+if(GSL_FOUND)
+  add_definitions(${CMAKE_GSL_CXX_FLAGS})
+  include_directories(${GSL_INCLUDE_DIR})
+  link_directories(${GSL_LINK_DIRECTORIES})
+else(GSL_FOUND)
+  if(ARGOS_BUILD_FOR_SIMULATOR)
+    message(FATAL_ERROR "When compiling the ARGoS simulator, GSL is required")
+  endif(ARGOS_BUILD_FOR_SIMULATOR)
+endif(GSL_FOUND)
+
+#
+# Check for FreeImage
+# It is required only when compiling the simulator
+#
+if(ARGOS_BUILD_FOR_SIMULATOR)
+  find_package(FreeImage)
+  if(NOT FREEIMAGE_FOUND)
+    message(FATAL_ERROR "Required library FreeImage not found.")
+  endif(NOT FREEIMAGE_FOUND)
+  include_directories(${FREEIMAGE_INCLUDE_PATH})
+endif(ARGOS_BUILD_FOR_SIMULATOR)
--- src/cmake/ARGoSBuildOptions.cmake
+++ src/cmake/ARGoSBuildOptions.cmake
@@ -5,6 +5,9 @@
 if(NOT DEFINED ARGOS_BUILD_FOR)
   # Variable was not set, set to default value
   set(ARGOS_BUILD_FOR "simulator" CACHE STRING "What is ARGoS being built for? \"simulator\" or a robot name (lowercase)")
+else(NOT DEFINED ARGOS_BUILD_FOR)
+  # Variable was set, make it public
+  set(ARGOS_BUILD_FOR ${ARGOS_BUILD_FOR} CACHE STRING "What is ARGoS being built for? \"simulator\" or a robot name (lowercase)")
 endif(NOT DEFINED ARGOS_BUILD_FOR)
 # Set a macro according to value set in ARGOS_BUILD_FOR
 add_definitions(-DARGOS_${ARGOS_BUILD_FOR}_BUILD)
