2832d2ceb578c374073cdaa28d693991bf47cf7c
Carlo Pinciroli <cpinciro@ulb.ac.be>
2013-05-08 10:45:56 +0200

    Fixed function for the foot-bot proximity sensor

--- src/plugins/robots/foot-bot/simulator/footbot_proximity_default_sensor.cpp
+++ src/plugins/robots/foot-bot/simulator/footbot_proximity_default_sensor.cpp
@@ -20,11 +20,11 @@ namespace argos {
    public:
 
       virtual Real CalculateReading(Real f_distance) {
-         if(f_distance < 0.00880828) {
+         if(f_distance < 0.009889556) {
             return 1.0;
          }
          else {
-            return 0.0100527 / (f_distance + 0.000163144) - 0.120523;
+            return 0.0100527 / (f_distance + 0.000163144);
          }
       }
 
