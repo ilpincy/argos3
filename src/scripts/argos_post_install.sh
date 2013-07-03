68dd420b3c128d2fe9e570d0e50c6506223ee0fd
Carlo Pinciroli <ilpincy@gmail.com>
2013-07-03 22:59:31 +0200

    Added initial support for .desktop entry and MIME type registration under freedesktop-compliant window managers

--- src/scripts/argos_post_install.sh
+++ src/scripts/argos_post_install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -e
+
+# Update library paths
 sudo /sbin/ldconfig
+
+# Update MIME type database
+sudo /usr/bin/update-mime-database "@CMAKE_INSTALL_PREFIX@/share/mime/"
+
+# Update desktop entry database
+sudo /usr/bin/update-desktop-database
