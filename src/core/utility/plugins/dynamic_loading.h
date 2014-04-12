97aa17624a6fac86b8a054ed492ab187aeb7310f
Carlo Pinciroli <cpinciro@ulb.ac.be>
2014-04-12 13:19:29 +0200

    Now dynamic libraries are loaded trying out multiple extensions for shared libs and modules

--- src/core/utility/plugins/dynamic_loading.h
+++ src/core/utility/plugins/dynamic_loading.h
@@ -33,32 +33,55 @@ namespace argos {
    public:
 
       /**
-       * Loads a dynamic library
+       * Loads a dynamic library.
+       * This method tries to load the library pointed to by the specified path.
+       * <p>
+       * If the given path is absolute, this method tries to load the library
+       * pointed to by the specified path. If loading fails, it tries another time
+       * appending the extension for shared libraries (e.g., <tt>.so</tt> on
+       * Linux, <tt>.dylib</tt> on Mac) to the path. If also this attempt fails,
+       * this method tries appending the extension for module libraries (e.g.,
+       * <tt>.so</tt> on Linux and Mac). If all attempts fail, the method raises
+       * an exception.
+       * </p>
+       * <p>
+       * If the given path is relative, this method forms absolute paths
+       * prepending the given path with the directories of the default loading
+       * path (e.g., /usr/lib/argos3), the contents of ARGOS_PLUGIN_PATH, and
+       * the current working directory. For each absolute path so formed, the
+       * method attempts to load the library in the same way as described above
+       * (e.g., appending the extension of shared/module libraries if needed).
+       * </p>
        * @param str_lib The path of the dynamic library to load
        * @return A handle to the loaded dynamic library
+       * @throws CARGoSException in case of error
        */
       static TDLHandle LoadLibrary(const std::string& str_lib);
 
       /**
-       * Unloads a dynamic library
+       * Unloads a dynamic library.
        * @param str_lib The path of the dynamic library to unload
+       * @throws CARGoSException in case of error
        */
       static void UnloadLibrary(const std::string& str_lib);
 
       /**
-       * Unloads a dynamic library
+       * Unloads a dynamic library.
        * @param t_lib The handle to the dynamic library to unload
+       * @throws CARGoSException in case of error
        */
       static void UnloadLibrary(TDLHandle t_lib);
 
       /**
-       * Loads all the dynamic libraries in the current ARGOS_PLUGIN_PATH
-       * Internally used to execute queries (<tt>argos3 --query</tt>)
+       * Loads all the dynamic libraries in the current ARGOS_PLUGIN_PATH.
+       * Internally used to execute queries (<tt>argos3 --query</tt>).
+       * @throws CARGoSException in case of error
        */
       static void LoadAllLibraries();
 
       /**
        * Unloads all the dynamic libraries.
+       * @throws CARGoSException in case of error
        */
       static void UnloadAllLibraries();
 
