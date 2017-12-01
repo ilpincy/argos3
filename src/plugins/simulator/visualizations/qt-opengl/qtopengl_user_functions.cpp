4cb05e9c5f587085188a379ccd3c6ace49c452fd
Alan Millard <alan.millard@york.ac.uk>
2017-12-01 21:18:45 +0000

    Added missing alpha value to pfColor in CQTOpenGLUserFunctions::SetColor()

--- src/plugins/simulator/visualizations/qt-opengl/qtopengl_user_functions.cpp
+++ src/plugins/simulator/visualizations/qt-opengl/qtopengl_user_functions.cpp
@@ -115,7 +115,8 @@ namespace argos {
       const GLfloat pfColor[]     = {
          c_color.GetRed()   / 255.0f,
          c_color.GetGreen() / 255.0f,
-         c_color.GetBlue()  / 255.0f
+         c_color.GetBlue()  / 255.0f,
+         1.0f
       };
       glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, DEFAULT_SPECULAR);
       glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, DEFAULT_SHININESS);
