00174021fbcb7f6206b5aed55fe6ade1dec8031f
Michael Allwright <allsey87@gmail.com>
2018-04-20 18:13:10 +0200

    Fixed bug in the three-dimensional dynamics magnetism plug-in

--- src/plugins/simulator/physics_engines/dynamics3d/dynamics3d_magnetism_plugin.cpp
+++ src/plugins/simulator/physics_engines/dynamics3d/dynamics3d_magnetism_plugin.cpp
@@ -11,6 +11,9 @@
 #include <argos3/plugins/simulator/physics_engines/dynamics3d/dynamics3d_model.h>
 #include <algorithm>
 
+#include <argos3/plugins/simulator/physics_engines/dynamics3d/dynamics3d_multi_body_object_model.h>
+#include <argos3/plugins/simulator/physics_engines/dynamics3d/bullet/BulletDynamics/Featherstone/btMultiBodyLink.h>
+
 namespace argos {
 
    /****************************************/
@@ -92,28 +95,28 @@ namespace argos {
                continue;
             }
             /* perform Barnes-hut algorithm */
-            /* calculate the normalized seperation between the two dipoles */
+            /* calculate the normalized seperation between the two dipoles, pointing from Dipole1 to Dipole0*/
             const btVector3& cNormalizedSeparation =
                btVector3(cPositionDipole0 - cPositionDipole1) / fDistance;
             /* calculate the rotated field of dipole 0 */
             const CVector3& cFieldDipole0 = itDipole0->Magnet->GetField();
             const btVector3& cRotatedFieldDipole0 = itDipole0->Body->GetTransform().getBasis() *
                btVector3(cFieldDipole0.GetX(), cFieldDipole0.GetZ(), -cFieldDipole0.GetY());
+            /* calculate the rotated field of dipole 1 */
+            const CVector3& cFieldDipole1 = itDipole1->Magnet->GetField();
+            const btVector3& cRotatedFieldDipole1 = itDipole1->Body->GetTransform().getBasis() *
+               btVector3(cFieldDipole1.GetX(), cFieldDipole1.GetZ(), -cFieldDipole1.GetY());
             /* We now have cRotatedFieldDipole0 and cRotatedFieldDipole1 as the magnetic moments
                (i.e., m0, m1), cNormalizedSeparation as the direction unit vector from Dipole 1
                to Dipole 0 (i.e., n), fDistance is the scalar distance between the dipoles (i.e.,
                d), and B0 is the magnetic flux density at Dipole 0.
                   B0 = u0/4pi * [3n(n.m1) - m1] / d^3
                   T0 = m0 * B0
-                     = u0.4pi/d^3 * [3 (m0.n)(m1 * n) - m0 * m1]
+                     = u0.4pi/d^3 * [3 (m1.n)(m0 * n) - m0 * m1]
 
                   F0 = grad(m0.B0)
                      = u0.4pi/d^4 * [-15n(m0.n)(m1.n) + 3n(m0.m1) + 3(m0(m1.n)+m1(m0.n))]
             */
-            /* calculate the rotated field of dipole 1 */
-            const CVector3& cFieldDipole1 = itDipole1->Magnet->GetField();
-            const btVector3& cRotatedFieldDipole1 = itDipole1->Body->GetTransform().getBasis() *
-               btVector3(cFieldDipole1.GetX(), cFieldDipole1.GetZ(), -cFieldDipole1.GetY());
             /* calculate the intermediate cross and dot products */
             const btVector3& cCrossProduct01 = cRotatedFieldDipole0.cross(cRotatedFieldDipole1);
             const btVector3& cCrossProduct0 = cRotatedFieldDipole0.cross(cNormalizedSeparation);
@@ -123,10 +126,10 @@ namespace argos {
             btScalar fDotProduct1 = cRotatedFieldDipole1.dot(cNormalizedSeparation);
             /* calculate the magnetic force and torque */
             const btVector3& cTorque0 =
-               ((3 * fDotProduct0 * cCrossProduct1) - cCrossProduct01) *
+               ((3 * fDotProduct1 * cCrossProduct0) - cCrossProduct01) *
                m_fForceConstant / btPow(fDistance, 3);
             const btVector3& cTorque1 =
-               ((3 * fDotProduct1 * cCrossProduct0) - cCrossProduct01) *
+               ((3 * fDotProduct0 * cCrossProduct1) + cCrossProduct01) *
                m_fForceConstant / btPow(fDistance, 3);
             const btVector3& cForce = (m_fForceConstant / btPow(fDistance, 4)) *
                ((-15 * cNormalizedSeparation * fDotProduct1 * fDotProduct0) +
