76a5335ff36552f42578e71727ca029249d2c18f
Michael Allwright <allsey87@gmail.com>
2018-06-22 15:42:03 +0200

    Disabled SRoCS example

--- src/testing/examples/prototype_srocs_system_arcontrol.lua
+++ src/testing/examples/prototype_srocs_system_arcontrol.lua
@@ -2,6 +2,7 @@ state = "begin"
 timer = 0
 
 function init()
+   logerr("[NOTE] This example requires recalibration and is disabled")
 	reset()
 end
 
@@ -20,7 +21,7 @@ function step()
 	-- run finite state machine
 	if state == "begin" then
 		state = 	"move_forwards"
-		set_speed(5,5)
+		set_speed(1,1)
 	elseif state == "move_forwards" then
 		regulate_end_effector_position(0.1)
 		if(robot.proximity[6] > 0.98 and robot.proximity[7] > 0.98) then
@@ -28,30 +29,30 @@ function step()
 			set_speed(0,0)
 		end
 	elseif state == "lower_end_effector" then
-		regulate_end_effector_position(0.0)
+		regulate_end_effector_position(0.005)
 		if(robot.proximity[14] > 0.99) then
 			state = "pick_up_block"
 			robot.joints.lift_fixture_vertical_link.set_target(0.0)
-   		robot.magnets.magnet_0.set_current(3)
-	   	robot.magnets.magnet_1.set_current(3)
-			robot.magnets.magnet_2.set_current(3)
-			robot.magnets.magnet_3.set_current(3)
+   		robot.magnets.magnet_0.set_current(1)
+	   	robot.magnets.magnet_1.set_current(1)
+			robot.magnets.magnet_2.set_current(1)
+			robot.magnets.magnet_3.set_current(1)
 			timer = 20;
 		end
 	elseif state == "pick_up_block" then
 		if(timer == 0) then
 			state = "raise_block"
-   		robot.magnets.magnet_0.set_current(1)
-	   	robot.magnets.magnet_1.set_current(1)
-			robot.magnets.magnet_2.set_current(1)
-			robot.magnets.magnet_3.set_current(1)
+   		robot.magnets.magnet_0.set_current(0.5)
+	   	robot.magnets.magnet_1.set_current(0.5)
+			robot.magnets.magnet_2.set_current(0.5)
+			robot.magnets.magnet_3.set_current(0.5)
 		end
 	elseif state == "raise_block" then
 		regulate_end_effector_position(0.0825)
 		if(robot.joints.lift_fixture_vertical_link.encoder > 0.060) then
 			state = "approach_block"
 			robot.radios.radio.tx_data({1});
-			set_speed(3,3)
+			set_speed(0.5,0.5)
 		end
 	elseif state == "approach_block" then
 		regulate_end_effector_position(0.0825)
