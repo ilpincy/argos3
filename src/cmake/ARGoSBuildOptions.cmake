32c42b0760f13df9cb35d68e02f491beb93dbfb0
Carlo Pinciroli <carlo@pinciroli.net>
2021-07-22 19:09:50 -0400

    Fixed the location of ARGOS_BUILD_FOR

--- src/cmake/ARGoSBuildOptions.cmake
+++ src/cmake/ARGoSBuildOptions.cmake
@@ -1,25 +1,4 @@
 #
-# What is ARGoS being built for?
-# Accepted values: "simulator" or a robot name (lowercase)
-#
-if(NOT DEFINED ARGOS_BUILD_FOR)
-  # Variable was not set, set to default value
-  set(ARGOS_BUILD_FOR "simulator" CACHE STRING "What is ARGoS being built for? \"simulator\" or a robot name (lowercase)")
-else(NOT DEFINED ARGOS_BUILD_FOR)
-  # Variable was set, make it public
-  set(ARGOS_BUILD_FOR ${ARGOS_BUILD_FOR} CACHE STRING "What is ARGoS being built for? \"simulator\" or a robot name (lowercase)")
-endif(NOT DEFINED ARGOS_BUILD_FOR)
-mark_as_advanced(ARGOS_BUILD_FOR)
-# Set a macro according to value set in ARGOS_BUILD_FOR
-add_definitions(-DARGOS_${ARGOS_BUILD_FOR}_BUILD)
-# Create a convenience variable for checks in the CMake files
-if(ARGOS_BUILD_FOR STREQUAL "simulator")
-  set(ARGOS_BUILD_FOR_SIMULATOR TRUE)
-else(ARGOS_BUILD_FOR STREQUAL "simulator")
-  set(ARGOS_BUILD_FOR_SIMULATOR FALSE)
-endif(ARGOS_BUILD_FOR STREQUAL "simulator")
-
-#
 # Install ld.so.conf?
 #
 if(NOT DEFINED ARGOS_INSTALL_LDSOCONF)
