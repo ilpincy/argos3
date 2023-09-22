f799ac2569c213bccb8b6d81ae72ef3f1146e46a
Carlo Pinciroli <carlo@pinciroli.net>
2023-09-22 14:08:59 -0400

    Added missing includes

--- src/core/utility/math/transforms.h
+++ src/core/utility/math/transforms.h
@@ -1,6 +1,9 @@
 #ifndef TRANSFORMS_H
 #define TRANSFORMS_H
 
+#include <argos3/core/utility/math/vector3.h>
+#include <argos3/core/utility/math/quaternion.h>
+
 namespace argos {
 
    CVector3 GlobalToLocal(const CVector3& c_global,
